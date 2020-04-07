0df42cb95b6b8bf940fe290700fe55416cc21abe
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-04-07 21:23:12 +0500

    Complete✌

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Container from 'components/Container';
 import Map from 'components/Map';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -34,8 +34,82 @@ const IndexPage = () => {
       return;
     }
 
-    console.log('response', response);
+    const { data } = resposne;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if ( !hasData ) return;
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [ lng, lat ]
+          }
+        }
+      })
+    }
+
+    // const geoJsonLayers = new L.GeoJSON(geoJson, {
+    //   pointToLayer: (feature = {}, latlng) => {
+      function countryPointToLayer (feature = {}, lasting) {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
     
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+    
+        casesString = `${cases}`;
+    
+        if ( cases > 1000 ) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+    
+        if ( updated ) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+    
+        const html = `
+          <span class=“icon-marker”>
+            <span class=“icon-marker-tooltip”>
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>
+        `;
+    
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: countryPointToLayer
+    });
+
+    geoJsonLayers.addTo(map);
   }
 
   const mapSettings = {
